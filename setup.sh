702d14aac9ff22d3e1cb109fb2b79e0fac25d842
Dilanka Kaushal Hewage <66146701+k4u5h41@users.noreply.github.com>
2022-10-23 11:30:01 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,11 @@
 
+sudo pacman -S git wget curl firefox mpv
+
+mkdir Tools/
+cd Tools/
+git clone https://aur.archlinux.org/paru-bin
+cd paru-bin/
+makepkg -si
+
+cd ~
+paru -S sublime-text code terminator zsh tmux arc-gtk-theme arc-icon-theme papirus-icon-theme
